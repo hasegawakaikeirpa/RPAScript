70461c8772aab5e4511103a1de0f0eeff2c4c5c8
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-02-22 13:18:57 +0900

    komit

---
+++ SQLConnect.py
@@ -0,0 +1,4 @@
+﻿def MySQLFind(user):#host='ws77',user='SYSTEM',passwd='SYSTEM',port='3306',database='test_db'
+ conn = MySQLdb.connect(host='ws77',user='SYSTEM',passwd='SYSTEM',port='3306',database='test_db')
+ conn.close
+import MySQLdb
