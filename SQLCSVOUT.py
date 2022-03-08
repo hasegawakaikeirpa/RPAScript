90643988310464ee10a49ad4670fd4bc2b7a513a
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-03-08 15:49:27 +0900

    komit

---
+++ SQLCSVOUT.py
@@ -0,0 +1,10 @@
+﻿import SQLConnect as SQ
+import pandas as pd
+
+sql = 'SELECT * FROM m_kkanyo'
+URL = "\\Sv05121a\e\C 作業台\RPA\ALLDataBase\m_kkanyo.csv"
+URL = URL.replace("\\","/")
+URL = "/" + URL
+SQDF = SQ.MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]
+SQDF.to_csv(URL, index = False)
+print(SQDF)
