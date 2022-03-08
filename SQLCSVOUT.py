32dd22a61f71ed6e2bdaa6abebc5d37b956c44a8
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-03-08 18:54:51 +0900

    komit

--- SQLCSVOUT.py
+++ SQLCSVOUT.py
@@ -1,10 +1,16 @@
 ﻿import SQLConnect as SQ
 import pandas as pd
-
+#関与先データベースをCSVアウト-------------------------------------------------------
 sql = 'SELECT * FROM m_kkanyo'
 URL = "\\Sv05121a\e\C 作業台\RPA\ALLDataBase\m_kkanyo.csv"
 URL = URL.replace("\\","/")
 URL = "/" + URL
 SQDF = SQ.MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]
 SQDF.to_csv(URL, index = False)
-print(SQDF)
+#社員情報をCSVアウト----------------------------------------------------------------
+sql = 'SELECT * FROM m_syain'
+URL = "\\Sv05121a\e\C 作業台\RPA\ALLDataBase\m_syain.csv"
+URL = URL.replace("\\","/")
+URL = "/" + URL
+SQDF = SQ.MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]
+SQDF.to_csv(URL, index = False)
