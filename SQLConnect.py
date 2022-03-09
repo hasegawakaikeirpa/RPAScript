36a5e53fc5cb4752985a573b02d317935aa415d4
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-03-09 17:24:12 +0900

    komit

--- SQLConnect.py
+++ SQLConnect.py
@@ -9,6 +9,8 @@
         conn.close()
         return True,SqlData
     except:
+        cursor.close()
+        conn.close()
         return False,"取得失敗"
 def MySQLHeaderTo_df(str_host,str_user,str_passwd,int_port,str_database,str_charset,sql):#host='ws77',user='SYSTEM',passwd='SYSTEM',port='3306',database='test_db',charset='utf8'
     try:
@@ -19,39 +21,98 @@ def MySQLHeaderTo_df(str_host,str_user,str_passwd,int_port,str_database,str_char
         conn.close()
         return True,SqlData
     except:
+        cursor.close()
+        conn.close()
         return False,"取得失敗"
+def MySQLAct(str_host,str_user,str_passwd,int_port,str_database,str_charset,sql):#host='ws77',user='SYSTEM',passwd='SYSTEM',port='3306',database='test_db',charset='utf8'
+    try:
+        conn = MySQLdb.connect(host=str_host,user=str_user,passwd=str_passwd,port=int_port,database=str_database,charset=str_charset)
+        cursor = conn.cursor()
+        cursor.execute(sql)  # select文を実行
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    except:
+        cursor.close()
+        conn.close()
+        return False
 
 import MySQLdb
 import pandas as pd
 import CSVOut as co
 import numpy as np
 import os
+from datetime import datetime as dt
 from tqdm import tqdm
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
-# sqlstr = 'SELECT * FROM m_kkanyo'
-# Sqldf = MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sqlstr)[1]
-# Sqldf.to_csv("//Sv05121a/e/C 作業台/RPA/ALLDataBase/TBCopyCopy.csv",index=None)
-# CSVList = co.CsvRead("//Sv05121a/e/C 作業台/RPA/ALLDataBase/TB.csv")
-# MasRow = np.array(Sqldf.shape[0])#配列行数取得
-# for x in range(MasRow):
-#     Sqldfrow = Sqldf.iloc[x,:]
-#     SyanaiCode = Sqldfrow['vc_KnrCd']
-#     CsvRow = co.CsvSortArray("//Sv05121a/e/C 作業台/RPA/ALLDataBase/TB.csv","SyanaiCode",int(SyanaiCode),"int")
-#     if CsvRow[0] == True:
-#         CsvRowRow = CsvRow[1]
-#         try:
-#             Siki = str(int(CsvRowRow['（国税）利用者識別番号']))
-#         except:
-#             Siki = str(CsvRowRow['（国税）利用者識別番号'])
-#         ID = str(CsvRowRow['（地方税）利用者ID'])
-#         #'vc_Msikibetu','vc_Mid'
-#         sql = "UPDATE m_kkanyo SET vc_Msikibetu = '" + Siki + "',vc_Mid = '" + ID + "' WHERE vc_KnrCd = '" + SyanaiCode + "'"
-#         #sql = 'UPDATE m_kkanyo SET vc_Msikibetu = ' + '12345678910' + ',vc_Mid = ' + 'hsaaha123456798' + ' WHERE vc_KnrCd = ' + SyanaiCode
-#         MSGet = MySQLGet('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql) #host='ws77',user='SYSTEM',passwd='SYSTEM',port='3306',database='test_db',charset='utf8'
-#         if MSGet[0] == True:
-#             print(SyanaiCode + "成功")
-#         else:
-#             print(SyanaiCode + "失敗")
+import datetime
+
+def MysqlDiffUp(MTB,CTB,UDRow):#元TB名,履歴TB名,更新行データ
+    try:
+        sql = 'SELECT * FROM ' + MTB #SELECT分を代入
+        KFM = MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]#関数でクエリ結果をDF化
+        DFCol = KFM.columns#DF列名を取得
+        ColList = []
+        ParList = []
+        for DFColItem in DFCol:#Rowデータのデータ型に併せて値を変更
+            ColList.append(DFColItem)
+        ColRow = 0
+        for UDRowItem in UDRow:
+            ColN = ColList[ColRow]
+            #データ型に合わせて値を変更----------------------------------------------------------------
+            if 'dt_' in ColN:
+                if UDRowItem == None:#Timestamp('2022-03-09 00:00:00')
+                    FDATE = datetime.datetime.now()
+                    FDATE = str(FDATE.strftime('%Y-%m-%d %H:%M:%S'))
+                    # FDATE = "Timestamp('" + FDATE + "')"
+                    ParList.append(FDATE)
+                else:
+                    ParList.append(UDRowItem)
+            else:
+                if UDRowItem == " ":
+                    ParList.append("")
+                elif UDRowItem == "　":
+                    ParList.append("")
+                elif UDRowItem == "  ":
+                    ParList.append("")
+                elif ColN == 'vc_KnrCd':
+                    ParList.append(UDRowItem.replace("  ","").replace(" ",""))
+                elif ColN == 'in_RrkNo_pk':
+                    ParList.append(UDRowItem + 1)
+                elif ColN == 'cr_RecKbn':
+                    ParList.append('1')
+                else:
+                    ParList.append(UDRowItem)
+            ColRow = ColRow + 1
+            #---------------------------------------------------------------------------------------
+        FColList = str(ColList)#SQL文用列名リストの作成
+        FColList = FColList.replace("'","")#不要なのでシングルコーテーション削除
+        sql = 'INSERT INTO ' + ITB + 'SELECT * FROM ' + MTB + ' WHERE vc_KnrCd=' + KFMData['vc_KnrCd'].replace("  ","").replace(" ","")#変更対象を履歴DBにコピー
+        MySQLAct('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)#引数8=親TB名,引数9=履歴数列名   
+        sql = 'insert into ' + ITB + ' (' + str(FColList) + ') values (' + str(ParList) + ')'
+        sql = sql.replace("[","").replace("]","")
+        MySQLAct('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)#引数8=親TB名,引数9=履歴数列名       
+        sql = 'SELECT * FROM m_kfmsmail WHERE vc_KnrCd=' + KFMData['vc_KnrCd'].replace("  ","").replace(" ","")
+        MFM = MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]#関数でクエリ結果をDF化
+        sql = 'SELECT * FROM m_kfmsmailrireki AS m WHERE NOT EXISTS (SELECT 1 FROM m_kfmsmailrireki AS s WHERE m.vc_KnrCd = s.vc_KnrCd AND m.in_RrkNo_pk < s.in_RrkNo_pk)'
+        KFMKFM = MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]#関数でクエリ結果をDF化
+        print(MFM)
+        print(KFMKFM)
+        print(KFMKFM[~KFMKFM.isin(MFM.to_dict(orient='list')).all(1)])
+        sql = ''
+        return True
+    except:
+        return False
+sql = 'SELECT * FROM m_kfmsmail'#SELECT分を代入
+KFM = MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]#関数でクエリ結果をDF化
+KFMRow = np.array(KFM).shape[0]#DF行数取得
+
+for x in range(KFMRow):#DF分ループ
+    KFMData = KFM.iloc[x,:]#DF行データ取得
+    ITB = 'm_kfmsmailrireki'#テーブル名を代入
+    ColRow = 0
+    MysqlDiffUp('m_kfmsmail',ITB,KFMData) #元TB名,履歴TB名,更新行データ
