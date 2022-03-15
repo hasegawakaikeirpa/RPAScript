b83ff0fc7579f71f22bf78fc8cf30a9d7426b149
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-03-15 09:44:30 +0900

    komit

--- SQLCSVOUT.py
+++ SQLCSVOUT.py
@@ -7,6 +7,13 @@ URL = URL.replace("\\","/")
 URL = "/" + URL
 SQDF = SQ.MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'test_db','utf8',sql)[1]
 SQDF.to_csv(URL, index = False)
+#HeidiデータベースをCSVアウト-------------------------------------------------------
+sql = 'SELECT * FROM kanyodb'
+URL = "\\Sv05121a\e\C 作業台\RPA\ALLDataBase\Heidi関与先DB.csv"
+URL = URL.replace("\\","/")
+URL = "/" + URL
+SQDF = SQ.MySQLHeaderTo_df('ws77','SYSTEM','SYSTEM',3306,'kanyodb','utf8',sql)[1]
+SQDF.to_csv(URL, index = False)
 #社員情報をCSVアウト----------------------------------------------------------------
 sql = 'SELECT * FROM m_syain'
 URL = "\\Sv05121a\e\C 作業台\RPA\ALLDataBase\m_syain.csv"
