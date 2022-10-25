ab61d315ee7cd513070c1f236c955b16d78a95a1
githubhasegawakaikeirpa <githubokimoto-t@hasegawakaikei.com>
2022-10-26 08:32:18 +0900

    ws247Commit

--- py_windriver_test00.py
+++ py_windriver_test00.py
@@ -3,14 +3,13 @@ from appium import webdriver
 
 
 class SampleTests(unittest.TestCase):
-
     def setUp(self):
         # set up appium
         desired_caps = {}
         desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
         self.driver = webdriver.Remote(
-            command_executor='http://127.0.0.1:4723',
-            desired_capabilities=desired_caps)
+            command_executor="http://127.0.0.1:4723", desired_capabilities=desired_caps
+        )
 
     def tearDown(self):
         self.driver.quit()
@@ -19,8 +18,7 @@ class SampleTests(unittest.TestCase):
         self.driver.find_element_by_accessibility_id("clearButton").click()
         self.driver.find_element_by_accessibility_id("num7Button").click()
 
-        result = self.driver.find_element_by_accessibility_id(
-            "CalculatorResults")
+        result = self.driver.find_element_by_accessibility_id("CalculatorResults")
         self.assertIn(" 7 ", str(result.text))
 
     def test_calc(self):
@@ -29,11 +27,10 @@ class SampleTests(unittest.TestCase):
         self.driver.find_element_by_accessibility_id("num7Button").click()
         self.driver.find_element_by_accessibility_id("equalButton").click()
 
-        result = self.driver.find_element_by_accessibility_id(
-            "CalculatorResults")
+        result = self.driver.find_element_by_accessibility_id("CalculatorResults")
         self.assertIn(" 9 ", str(result.text))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(SampleTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
